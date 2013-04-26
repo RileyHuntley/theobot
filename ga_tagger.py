796ad605fb3df005f1619de72760c60f403263ac
Riley <wikihuntley@gmail.com>
2013-04-26 14:24:51 -0700

    Update ga_tagger.py
    argh idfk

--- ga_tagger.py
+++ ga_tagger.py
@@ -6,107 +6,113 @@ import sys
 # CC-BY-SA Theopolisme
 
 def cats_recursive(category):
-	"""Recursively goes through
-	categories. Almost TOO
-	straightforward.
-	
-	Note that category must be
-	a STRING.
-	"""
-	pages = []
-	
-	category = mwclient.listing.Category(site, category)
-	print category
-	
-	for item in category:
-		if "Category:" in unicode(item):
-			cats_recursive(item)
-		else:
-			x = item.strip_namespace(item.name)
-			print x.encode('UTF-8', 'ignore')
-			pages.append(x)
-	
-	return pages
+    """Recursively goes through
+    categories. Almost TOO
+    straightforward.
+
+    Note that category must be
+    a STRING.
+    """
+    pages = []
+
+    category = mwclient.listing.Category(site, category)
+    print category
+
+    for item in category:
+	    if "Category:" in unicode(item):
+		    cats_recursive(item)
+	    else:
+		    x = item.strip_namespace(item.name)
+		    print x.encode('UTF-8', 'ignore')
+		    pages.append(x)
+
+    return pages
 
 def tagged_already(page):
-	"""Returns TRUE if page has been
-	tagged with {{Good article}} or
-	one of its redirects.
-	"""
-	check_page()
-	page = site.Pages[page]
-	text = page.edit()
-	
-	regexp1 = re.compile(r'\{\{([Gg]ood [Aa]rticle|GA [Aa]rticle)\}\}')
-
-	if regexp1.search(text) is not None:
-		return True
-	else:
-		return False
-
+    """Returns TRUE if page has been
+    tagged with {{Good article}} or
+    one of its redirects.
+    """
+    check_page()
+    page = site.Pages[page]
+    text = page.edit()
+    regexp1 = re.compile(r'\{\{([Gg]ood [Aa]rticle|GA [Aa]rticle)\}\}')
+    if regexp1.search(text) is not None:
+    	return True
+    else:
+    	return False
+def class_ga(talkpage):
+	regexp2 = re.compile(r'\|(\s*)class(\s*)=(\s*)GA')
+           if regexp2.search(text) is None:
+                    return False
+            else:
+                    return True
 def process_current_gas():
-	"""For all current GAs, makes sure
-	topicon is on article and, if not,
-	adds it.
-	"""
-	pages = cats_recursive('Category:Wikipedia good articles')
+    """For all current GAs, makes sure
+    topicon is on article and, if not,
+    adds it.
+    """
+    pages = cats_recursive('Category:Wikipedia good articles')
 
-	for talkpage in pages:
-		page = talkpage
-		print "Working on: " + page.encode('UTF-8', 'ignore')
-		if tagged_already(page) == False:
-			pagee = site.Pages[page]
-			text = pagee.edit()
-			text = "{{Good article}}\n" + text
-			pagee.save(text,summary="[[User:RileyBot|Bot]] trial: Adding {{[[Template:Good article|Good article]]}} topicon) ([[User:RileyBot/12|Task 12]] - [[User:RileyBot/Stop/12|disable]]")
-			global counts
-			counts['current-added'] = counts['current-added'] + 1
-			counts['total_edits'] = counts['total_edits'] + 1
-		else:
-			print "Page already tagged."
+    for talkpage in pages:
+	    page = talkpage
+	    print "Working on: " + page.encode('UTF-8', 'ignore')
+	    if tagged_already(page) == False:
+		    pagee = site.Pages[page]
+		    text = pagee.edit()
+		    text = "{{Good article}}\n" + text
+		    pagee.save(text,summary="[[User:RileyBot|Bot]] trial: Adding {{[[Template:Good article|Good article]]}} topicon) ([[User:RileyBot/12|Task 12]] - [[User:RileyBot/Stop/12|disable]]")
+		    global counts
+		    counts['current-added'] = counts['current-added'] + 1
+		    counts['total_edits'] = counts['total_edits'] + 1
+	    else:
+		    print "Page already tagged."
 
 def process_delisted_gas():
-	"""Removes topicon from delisted
-	or former GA nominees.
-	"""
-	pages = cats_recursive('Category:Delisted good articles')
-	pages += cats_recursive('Former good article nominees')
+    """Removes topicon from delisted
+    or former GA nominees.
+    """
+    pages = cats_recursive('Category:Delisted good articles')
+    pages += cats_recursive('Category:Former good article nominees')
+    
+    for page in pages:
+	    print "Working on: " + page.encode('UTF-8', 'ignore')
+	    if tagged_already(page) == True:
+	    	if class_ga(talkpage) == False:
+		    	pagee = site.Pages[page]
+		    	text = pagee.edit()
+		    	text_diff = pagee.edit()
+		    	text = re.sub(r'\{\{([Gg]ood [Aa]rticle|GA [Aa]rticle)\}\}', '', text)
+		    	if text != text_diff:
+			    	pagee.save(text,summary="[[User:RileyBot|Bot]] trial: Removing {{[[Template:Good article|Good article]]}} topicon) ([[User:RileyBot/12|Task 12]] - [[User:RileyBot/Stop/12|disable]]")
+			    	global counts
+			    	counts['former-removed'] = counts['former-removed'] + 1
+			    	counts['total_edits'] = counts['total_edits'] + 1
 
-	for page in pages:
-		print "Working on: " + page.encode('UTF-8', 'ignore')
-		if tagged_already(page) == True:
-			pagee = site.Pages[page]
-			text = pagee.edit()
-			text_diff = pagee.edit()
-			text = re.sub(r'\{\{([Gg]ood [Aa]rticle|GA [Aa]rticle)\}\}', '', text)
-			if text != text_diff:
-				pagee.save(text,summary="[[User:RileyBot|Bot]] trial: Removing {{[[Template:Good article|Good article]]}} topicon) ([[User:RileyBot/12|Task 12]] - [[User:RileyBot/Stop/12|disable]]")
-				global counts
-				counts['former-removed'] = counts['former-removed'] + 1
-				counts['total_edits'] = counts['total_edits'] + 1
-				
 def check_page():
     stop_page = site.Pages['User:RileyBot/Stop/12']
     stop_page_text = stop_page.edit()
     if stop_page_text.lower() != u'enable':
         print "Check page disabled"
         sys.exit(0)
-        
+
 def main():
-	"""This defines and fills a global
-	variable for the site, and then runs.
-	"""
-	print "Logging in as " + settings.username + "..."
-	
-	global site
-	site = mwclient.Site('en.wikipedia.org')
-	site.login(settings.username, settings.password)
-	check_page()
-	global counts
-	counts = {'total_edits': 0, 'current-added': 0, 'former-removed': 0}
-	process_current_gas()
-	process_delisted_gas()
-	print "TOTAL EDITS MADE: " + counts['total_edits'] + "\nADDED: " + counts['current-added'] + "\nREMOVED: " + counts['former-removed']
+    """This defines and fills a global
+    variable for the site, and then runs.
+    """
+    print "Logging in as " + settings.username + "..."
+
+    global site
+    site = mwclient.Site('en.wikipedia.org')
+    site.login(settings.username, settings.password)
+    check_page()
+    global counts
+    counts = {'total_edits': 0, 'current-added': 0, 'former-removed': 0}
+    process_delisted_gas()
+    process_current_gas()
+
+
+    print "TOTAL EDITS MADE: " + counts['total_edits'] + "\nADDED: " + counts['current-added'] + "\nREMOVED: " + counts['former-removed']
 
 if __name__ == '__main__':
-   main()
+    main()
